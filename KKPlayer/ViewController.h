3922f5ebe4ba1ea06b4348f55e19e520277f560f
zy <525153575@qq.com>
2017-08-23 22:48:43 +0800

    dev:kk

---
+++ KKPlayer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  KKPlayer
+//
+//  Created by 张毅 on 2017/8/23.
+//  Copyright © 2017年 zhangyi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
